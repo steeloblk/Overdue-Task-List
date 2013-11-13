f3f900a849363cee840da616a9a39077e9909c85
Warren Deshazo <warrendeshazo@Warrens-iMac.local>
2013-11-12 21:27:26 -0500

    Initial Commit

---
+++ Overdue Task List/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Overdue Task List
+//
+//  Created by Warren Deshazo on 11/12/13.
+//  Copyright (c) 2013 Circboxx LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Overdue Task List/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Overdue Task List
+//
+//  Created by Warren Deshazo on 11/12/13.
+//  Copyright (c) 2013 Circboxx LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
